3b6d6fc2650372252bcefa3f20607b3fef150623
RTae <potae02@gmail.com>
2020-11-05 01:50:22 +0700

    Add more

---
+++ stylelearn/src/components/core/Content.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="content">
+    <v-main>
+        <Home/>
+    </v-main>
+  </div>
+</template>
+
+<script>
+import Home from '../../views/Home'
+
+export default {
+  name: 'Content',
+  components: {
+    Home
+  }
+}
+</script>
---
+++ stylelearn/src/components/core/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="footer">
+    <v-footer
+        dark
+        padless
+      >
+        <v-card
+          class="flex"
+          flat
+          tile
+          color='font_b'
+        >
+          <v-card-text class="py-2 white--text text-center">
+            {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+          </v-card-text>
+        </v-card>
+    </v-footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
