742757bc8dd226d85a0860283bfaac55f6082b97
RTae <potae02@gmail.com>
2020-11-12 10:07:07 +0700

    Add more backend

--- backend/api/main.go
+++ backend/api/main.go
@@ -1,12 +1,40 @@
 package handler
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
-	"time"
+	"os"
+	"path/filepath"
 )
 
+type env struct {
+	Foo string `json: foo`
+	Bar string `json: bar`
+}
+
 func Handler(w http.ResponseWriter, r *http.Request) {
-	currentTime := time.Now().Format(time.RFC850)
-	fmt.Fprintf(w, currentTime)
+	w.Header().Set("Content-Type", "application/json")
+	f := os.Getenv("FOO")
+	b := os.Getenv("BAR")
+
+	e := env{
+		Foo: f,
+		Bar: b,
+	}
+	json.NewEncoder(w).Encode(e)
+
+}
+func file(w http.ResponseWriter) {
+	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+	fmt.Fprintf(w, dir)
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
 }
