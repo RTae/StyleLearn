58c3d7ae63806c6c40676573a168920081250488
first <thanakit.first@mail.kmutt.ac.th>
2020-11-17 12:42:44 +0700

    edit Tutor

--- stylelearn/src/views/Tutor/HomeTutor.vue
+++ stylelearn/src/views/Tutor/HomeTutor.vue
@@ -1,45 +1,117 @@
 <template>
   <v-container fluid class="main" id="upload">
-      <v-row style="margin-top:0vh" justify="center">
-        <router-link to="/uploadvideotutor">
-          <button class="UploadVideoBtn">+ Upload My Video</button>
-        </router-link>
-      </v-row>
-      <v-row align="center" justify="center">
-        <p class="homeTitle">My Video</p>
-      </v-row>
-      <v-row align="center" justify="center">
-        <router-link to="/myvideotutor">
-          <p> All</p>
-        </router-link>
-      </v-row>
+    <!--upload video button-->
+    <v-row style="margin-top:0vh" justify="center">
+      <router-link to="/uploadvideotutor">
+        <button class="UploadVideoBtn">+ Upload My Video</button>
+      </router-link>
+    </v-row>
+    <v-row align="center" justify="center">
+      <p class="homeTitle">My Video</p>
+    </v-row>
+    <v-row align="center" justify="center">
+      <router-link to="/myvideotutor">
+        <p class="text2">All</p>
+      </router-link>
+    </v-row>
+  <!--course picture 1-->
+    <v-row>
+      <v-col cols="3">
+        <v-card
+          width="300"
+          height="250px"
+          style="margin-left:400px"
+          color="#70CCFF"
+        >
+          <v-img
+            class="white--text align-end"
+            height="200px"
+            src="https://sv1.picz.in.th/images/2020/11/16/bHaFJ2.png"
+          >
+          </v-img>
+          <v-btn class="botton" color="#70CCFF" width="100%" height="50px">
+            16,215 Views
+          </v-btn>
+        </v-card>
+      </v-col>
+      <!--course picture 2-->
+      <v-col cols="3">
+        <v-card
+          width="300"
+          height="250px"
+          style="margin-left:400px"
+          color="#70CCFF"
+        >
+          <v-img
+            class="white--text align-end"
+            height="200px"
+            src="https://sv1.picz.in.th/images/2020/11/16/bHaFJ2.png"
+          >
+          </v-img>
+          <v-btn class="botton" color="#70CCFF" width="100%" height="50px">
+            14,1756 Views
+          </v-btn>
+        </v-card>
+      </v-col>
+      <!--course picture 3-->
+      <v-col cols="3">
+        <v-card
+          width="300"
+          height="250px"
+          style="margin-left:400px"
+          color="#70CCFF"
+        >
+          <v-img
+            class="white--text align-end"
+            height="200px"
+            src="https://sv1.picz.in.th/images/2020/11/16/bHaFJ2.png"
+          >
+          </v-img>
+          <v-btn class="botton" color="#70CCFF" width="100%" height="50px">
+            19,125 Views
+          </v-btn>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row align="center" justify="center">
+      <p class="text3">Lesson Name: Differential I</p>
+    </v-row>
+    <v-row>
+      <v-card
+        justify="center"
+        width="400px"
+        height="250px"
+        style="center"
+        border-radius="40px"
+        color="#70CCFF"
+      >
+      </v-card>
+    </v-row>
   </v-container>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
+<style scope>
 .UploadVideoBtn {
   background-color: #5cbbf6;
   font-family: "Average Sans", sans-serif;
   border-radius: 10px;
   margin-right: 1300px;
   margin-top: 100px;
-  width: 140px;
-  height: 45px;
+  width: 150px;
+  height: 55px;
   transition: 0.3s;
-  font-size: 13px;
+  font-size: 14px;
   color: white;
   box-shadow: 0 0 4px #999;
   cursor: pointer;
 }
 
 .homeTitle {
-  font-size: 30px;
+  font-size: 40px;
   font-weight: bold;
   font-family: "Average Sans", sans-serif;
   margin-right: 1300px;
@@ -47,7 +119,24 @@ export default {
 }
 
 .text {
-  font-size: 100px;
+  font-size: 13px;
 }
 
+.text2 {
+  font-size: 25px;
+  margin-left: 1500px;
+}
+
+.text3 {
+  font-size: 13px;
+  margin-right: 780px;
+}
+
+.botton {
+  display: block;
+  border-style: none;
+  position: absolute;
+  box-shadow: none;
+  background-repeat: no-repeat;
+}
 </style>
