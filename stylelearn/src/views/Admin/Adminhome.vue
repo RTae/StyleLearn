b14ecb3083ab02c101c3e4960d6121e26f496d3a
Mozzy26 <sahassawas.moth@gmail.com>
2020-11-19 03:40:28 +0700

    Create other page

---
+++ stylelearn/src/views/Admin/Adminhome.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-container fluid class="main" id="Admin">
+      <v-row></v-row>
+
+  </v-container>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
