8e6ba3f3af449a8de31448d4f2e1e360f0d878ef
first <thanakit.first@mail.kmutt.ac.th>
2020-11-19 17:09:11 +0700

    edit tutor

--- stylelearn/src/views/Admin/Adminhome.vue
+++ stylelearn/src/views/Admin/Adminhome.vue
@@ -1,26 +1,4 @@
 <template>
-<<<<<<< HEAD
-  <v-container fluid class="main" id="Admin">
-    <v-col>
-      <div>
-        <router-link to="/">
-          <button>Video</button>
-        </router-link>
-
-        <router-link to="/">
-          <button>Customer</button>
-        </router-link>
-
-        <router-link to="/">
-          <button>Approve video</button>
-        </router-link>
-
-        <router-link to="/">
-          <button>Approve payment</button>
-        </router-link>
-      </div>
-    </v-col>
-=======
   <v-container fluid class="main" id="adminhome">
     <!-- Subject title -->
     <v-row align="center" justify="center" style="margin-top: 40px">
@@ -81,14 +59,10 @@
         </div>
       </v-col>
     </v-row>
->>>>>>> 1bbfc8545fbbc638858eddd0fb5505ad84018924
   </v-container>
 </template>
 
 <script>
-<<<<<<< HEAD
-export default {};
-=======
 export default {
   name: "adminhome",
   components: {},
@@ -182,7 +156,6 @@ export default {
     }
   }
 };
->>>>>>> 1bbfc8545fbbc638858eddd0fb5505ad84018924
 </script>
 <style scoped>
 .main {
@@ -202,9 +175,6 @@ export default {
   border-radius: 30px;
 }
 
-<<<<<<< HEAD
-<style></style>
-=======
 .text {
   font-weight: normal;
   color: white;
@@ -235,4 +205,3 @@ export default {
   style: "margin-top: 40px";
 }
 </style>
->>>>>>> 1bbfc8545fbbc638858eddd0fb5505ad84018924
