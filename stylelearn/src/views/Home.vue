36966ceeaac2de669e594f7ad9c3b8812382f6e7
first <thanakit.first@mail.kmutt.ac.th>
2020-11-19 11:10:20 +0700

    edit tutor

--- stylelearn/src/views/Home.vue
+++ stylelearn/src/views/Home.vue
@@ -8,7 +8,7 @@
             :elevation="hover ? 8 : 16"
             :class="{ 'on-hover': hover }"
             class="courseCard"
-            @click="onClickCourse('CALCULUS I')"
+            @click="onClickCourse()"
           >
             <v-card-text class="cardTextTitle"> CALCULUS I </v-card-text>
           </v-card>
@@ -18,7 +18,7 @@
             :elevation="hover ? 8 : 16"
             :class="{ 'on-hover': hover }"
             class="courseCard"
-            @click="onClickCourse('CALCULUS II')"
+            @click="onClickCourse()"
           >
             <v-card-text class="cardTextTitle"> CALCULUS II </v-card-text>
           </v-card>
@@ -28,7 +28,7 @@
             :elevation="hover ? 8 : 16"
             :class="{ 'on-hover': hover }"
             class="courseCard"
-            @click="onClickCourse('CALCULUS III')"
+            @click="onClickCourse()"
           >
             <v-card-text class="cardTextTitle"> CALCULUS III </v-card-text>
           </v-card>
@@ -43,7 +43,7 @@
 
     <!-- Newest -->
     <v-row>
-      <v-col cols="6" >
+      <v-col cols="6">
         <div style="margin-left:50px">
           <p style="font-size: 50px" class="textDetail">
             Newest
@@ -410,16 +410,13 @@ export default {
     langauge: "https://cdn.pixabay.com/photo/2020/07/12/07/47/bee-5396362_1280.jpg",
     econ: "https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
   }),
-  mounted () {
-    this.$store.dispatch({ type: "enterHome" });
-  },
   methods: {
-    onClickCourse (name) {
-      this.$router.push({ name: "CoursesPage", params: { title: name } })
+    onClickCourse () {
+      this.$router.push("/coursespage")
     },
     onClickSubject (n) {
       this.model = n - 1;
-      this.$router.push({ name: "CoursesPage", params: { titleName: this.items[n - 1] } })
+      this.$router.push("/coursespage")
     }
   }
 };
