59a623d4b7c6915862447e410797aff37e95ab87
RTae <potae02@gmail.com>
2020-12-10 17:48:42 +0700

    Change backend

--- backend/helper/helper.go
+++ backend/helper/helper.go
@@ -1,9 +1,67 @@
 package helper
 
-import "net/http"
+import (
+
+	// Import GORM-related packages.
+
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
 
 func SetupResponse(w *http.ResponseWriter, req *http.Request) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
 	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+}
+
+func InitDB() (*gorm.DB, map[string]interface{}) {
+
+	dsn := os.Getenv("COCKROACHDB_URL")
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log := ErrorHandle(err)
+		return nil, log
+	}
+
+	log := make(map[string]interface{})
+	log["status"] = "1"
+	log["msg"] = ""
+	log["result"] = ""
+
+	return db, log
+}
+
+func ErrorHandle(err error) map[string]interface{} {
+	var textError string
+	var textStatus string
+	if err.Error() == "mongo: no documents in result" {
+		textError = "User not found"
+		textStatus = "215"
+	} else {
+		textError = err.Error()
+		textStatus = "415"
+	}
+
+	logs := make(map[string]interface{})
+	logs["status"] = textStatus
+	logs["msg"] = textError
+	logs["result"] = ""
+	return logs
+}
+
+func IncreaseID(id, name string, length int) (string, error) {
+	digit, err := strconv.Atoi(id[length:])
+	if err != nil {
+		return name, err
+	}
+	digit++
+	s := strconv.Itoa(digit)
+
+	newID := name + strings.Repeat("0", 10-length-len(s)) + s
+	return newID, nil
 }
